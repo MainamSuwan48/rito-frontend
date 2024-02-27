109914db6e85e8e587177565edfc8e94f3d9db56
MainamSuwan48 <112855044+SexyCornman@users.noreply.github.com>
2024-02-27 14:00:35 +0700

    added neccessary files

---
+++ src/features/auth/validations/validate-login.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+import validate from "";
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    "string.empty": "Please enter your email",
+    "any.required": "Please enter your email",
+  }),
+  password: Joi.string().min(6).required().messages({
+    "string.empty": "Please enter your password",
+    "string.min": "Password must be at least 6 characters",
+    "any.required": "Please enter your password",
+  }),
+});
+
+const validateLogin = (input) => validate(loginSchema)(input);
+export default validateLogin;
---
+++ src/features/auth/validations/validate-register.js
@@ -0,0 +1,38 @@
+import Joi from "joi";
+import validate from "../../../utils/validator";
+
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(30).required().trim().messages({
+    "string.empty": "Username is required.",
+    "string.min": "Username must be at least 3 characters long.",
+    "string.max": "Username cannot exceed 30 characters.",
+  }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .messages({
+      "string.empty": "Email is required.",
+      "string.email": "Email must be a valid email address.",
+    }),
+  password: Joi.string()
+    .pattern(
+      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/
+    )
+    .required()
+    .messages({
+      "string.empty": "Password is required.",
+      "string.pattern.base":
+        "Password must contain at least one letter, one number, and one special character and atleast 6 characters.",
+    }),
+  confirmPassword: Joi.string()
+    .valid(Joi.ref("password"))
+    .required()
+    .messages({
+      "string.empty": "Confirm password is required.",
+      "any.only": "Passwords do not match.",
+    })
+    .strip(),
+});
+
+const validateRegister = (input) => validate(registerSchema)(input);
+export default validateRegister;
