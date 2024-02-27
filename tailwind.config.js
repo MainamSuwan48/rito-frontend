52f3143bf836a87f4f53be0fd687e3892fa03068
MainamSuwan48 <112855044+SexyCornman@users.noreply.github.com>
2024-02-27 13:45:13 +0700

    added comment in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,30 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
     themes: [
+
+      ////////////////////////////////////////////// *************ADJUST YOUR THEME HERE********************* //////////////////////////////////////////////
       {
         mytheme: {
-        
-"primary": "#C850C0",
-        
-"secondary": "#FFCC70",
-        
-"accent": "#ec4899",
-        
-"neutral": "#ffffff",
-        
-"base-100": "#4158D0",
-        
-"info": "#fde047",
-        
-"success": "#84cc16",
-        
-"warning": "#f59e0b",
-        
-"error": "#dc2626",
+          primary: '#FFCC70',
+
+          secondary: '#4158D0',
+
+          accent: '#ec4899',
+
+          neutral: '#ffffff',
+
+          'base-100': '#C850C0',
+
+          info: '#fde047',
+
+          success: '#84cc16',
+
+          warning: '#f59e0b',
+
+          error: '#dc2626',
         },
       },
     ],
