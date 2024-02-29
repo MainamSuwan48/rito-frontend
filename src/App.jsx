f99def32a0ed2502ff7c8d233727b067be3696f9
MainamSuwan48 <mainamsuwan48@gmail.com>
2024-02-29 15:18:53 +0700

    prepare for branch merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
 import React from 'react';
+import RefferencePage from './pages/ReferencePage';
 
 function App() {
-  return (
-    <div className='flex h-screen flex-col items-center justify-center gap-2 bg-base_dark text-3xl font-black'>
-      <div className='m-2 flex items-center justify-center rounded-md bg-base p-6'>
-        <span className='animate-bounce text-5xl text-primary'>Rito</span>
-        <span className='text-secondary'>App</span>
-        <span className='text-neutral'>GameStore</span>
-      </div>
-      <div className='flex'>
-        <div className='border-2 bg-primary p-5 text-2xl'>Primary</div>
-        <div className='border-2 bg-secondary p-5 text-2xl'>secondary</div>
-        <div className='border-2 bg-neutral p-5 text-2xl'>neutral</div>
-        <div className='bg-base-dark border-2 p-5 text-2xl'>Base Dark</div>
-        <div className='border-2 bg-base p-5 text-2xl'>Base </div>
-        <div className='border-2 bg-base_light p-5 text-2xl'>Base Light</div>
-      </div>
-      <div className='flex'>
-        <div className='bg-primary_mute border-2 p-5 text-2xl'>Primary</div>
-        <div className='bg-secondary_mute border-2 p-5 text-2xl'>secondary</div>
-        <div className='bg-danger border-2 p-5 text-2xl'>Danger</div>
-        <div className='bg-success border-2 p-5 text-2xl'>Success</div>
-      </div>
-    </div>
-  );
+  return <RefferencePage />;
 }
 
 export default App;
