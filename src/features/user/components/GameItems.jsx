ab7655139f80a1e975830e3f94f85c81a4024074
“puttpoomgit config --global user.email “poom.bm@gmail.com <poom@poomMAC.local>
2024-03-04 18:08:53 +0700

    edited GameItems

--- src/features/user/components/GameItems.jsx
+++ src/features/user/components/GameItems.jsx
@@ -2,27 +2,32 @@ export default function GameItems() {
   return (
     <>
       <div className='inline-flex h-fit w-full items-center justify-between bg-zinc-100 pr-4 outline outline-black'>
-        <div className='inline-flex flex-col items-start justify-start gap-2.5'>
-          <img className='h-20 w-72' src='https://via.placeholder.com/286x85' />
-        </div>
-        <div className='inline-flex flex-col items-start justify-center gap-2.5'>
-          <div className='text-center  text-xl font-bold leading-normal text-black'>
-            Mario
+        <div className='flex gap-2.5'>
+          <div className='inline-flex flex-col items-start justify-start gap-2.5'>
+            <img
+              className='h-20 w-72'
+              src='https://via.placeholder.com/286x85'
+            />
           </div>
-          <div className='inline-flex items-start justify-start gap-2'>
-            <div className='flex items-center justify-end gap-2.5 bg-white px-4 py-1.5'>
-              <div className='text-center  text-sm font-normal leading-normal text-black'>
-                kids
-              </div>
+          <div className='inline-flex flex-col items-start justify-center gap-2.5'>
+            <div className='text-center  text-xl font-bold leading-normal text-black'>
+              Mario
             </div>
-            <div className='flex items-center justify-center gap-2.5 bg-white px-4 py-1.5'>
-              <div className='text-center  text-sm font-normal leading-normal text-black'>
-                open world
+            <div className='inline-flex items-start justify-start gap-2'>
+              <div className='flex items-center justify-end gap-2.5 bg-white px-4 py-1.5'>
+                <div className='text-center  text-sm font-normal leading-normal text-black'>
+                  kids
+                </div>
               </div>
-            </div>
-            <div className='flex items-center justify-center gap-2.5 bg-white px-4 py-1.5'>
-              <div className='text-center  text-sm font-normal leading-normal text-black'>
-                creativity
+              <div className='flex items-center justify-center gap-2.5 bg-white px-4 py-1.5'>
+                <div className='text-center  text-sm font-normal leading-normal text-black'>
+                  open world
+                </div>
+              </div>
+              <div className='flex items-center justify-center gap-2.5 bg-white px-4 py-1.5'>
+                <div className='text-center  text-sm font-normal leading-normal text-black'>
+                  creativity
+                </div>
               </div>
             </div>
           </div>
--- src/pages/UserPage/UserProfilePage.jsx
+++ src/pages/UserPage/UserProfilePage.jsx
@@ -1,11 +1,15 @@
 import BodyContainer from '@/features/user/components/BodyContainer';
 import UserProfileContainer from '@/features/user/components/UserProfileContainer';
+import Header from '@/layouts/Header';
 
 export default function UserProfilePage() {
   return (
-    <div className='container grid grid-flow-row gap-4'>
-      <UserProfileContainer />
-      <BodyContainer />
-    </div>
+    <>
+      <Header />
+      <div className='container grid grid-flow-row gap-4'>
+        <UserProfileContainer />
+        <BodyContainer />
+      </div>
+    </>
   );
 }
