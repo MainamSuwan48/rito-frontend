bed0802a6d864a2315e489d4fce1fe2777cb38de
MainamSuwan48 <mainamsuwan48@gmail.com>
2024-03-05 14:03:31 +0700

    added logic for gamecards

--- src/features/store/components/GameCardTag.jsx
+++ src/features/store/components/GameCardTag.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function GameCardTag({ children }) {
+function GameCardTag({ children , id}) {
   return (
-    <button className='border-2 border-black p-2 transition-all text-black hover:border-primary hover:scale-105 active:scale-100'>
+    <button className='flex justify-center items-center h-[2rem] border-2 border-black text-xs transition-all px-1 text-black hover:border-primary hover:scale-105 active:scale-100'>
       {children}
     </button>
   );
