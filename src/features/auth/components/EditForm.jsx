6c20e8ef3de9ce71a4e84b1f31ffb0de9821ccdb
bennytakdanai <takdanai.ht@gmail.com>
2024-03-06 18:35:15 +0700

    edit user style

--- src/features/auth/components/EditForm.jsx
+++ src/features/auth/components/EditForm.jsx
@@ -44,18 +44,18 @@ export function EditForm({user}) {
           console.log(err);
         })
         console.log(user)
-      }
+    }
   return (
     <Dialog>
       <DialogTrigger 
         asChild
-        className='h-fit text-center font-semibold  text-white gap-2.5 bg-pink-500 px-20 py-4 rounded-none'
+        className='h-fit text-center text-base font-semibold  text-white gap-2.5 bg-pink-500 px-20 py-4 rounded-none'
       >
         <Button variant="outline">Edit Profile</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[625px]">
+      <DialogContent className="sm:max-w-[525px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle className="text-2xl">Edit profile</DialogTitle>
           <DialogDescription>
             Make changes to your profile here. Click save when you're done.
           </DialogDescription>
@@ -63,76 +63,72 @@ export function EditForm({user}) {
         <form 
             onSubmit={handleSubmit(onSubmit)} 
             className='flex items-center justify-start gap-4'
-          >
-            <div className='inline-flex flex-col items-start justify-start gap-6'>
-              <div className='flex flex-col items-start justify-center gap-4'>
-                <div className='inline-flex items-start justify-center gap-2.5'>
-                    <div className='mb-2 font-semibold text-base_dark'>Display Name :</div>
+        >
+            <div className='inline-flex flex-col items-start justify-start mt-4 gap-6 w-[550px]'>
+                <div className='relative w-full'>
+                    <div className='flex-shrink-0 mb-2 font-semibold text-base_dark'>Display Name :</div>
                     <input
                       type='text'
                       {...register('displayName')}
-                      className='input-bordered rounded-md border-2 p-1.5 text-black'
+                      className='input-bordered rounded-md w-full border-2 p-1.5 text-black'
                     />
                     {errors['displayName'] && (
-                      <div className='mt-2 text-wrap text-xs font-semibold text-danger'>
+                      <div className=' text-wrap text-xs font-semibold text-danger'>
                         {errors['displayName'].message}
                       </div>
                     )}
                 </div>
-                <div className='inline-flex items-center justify-start gap-2'>
-                  <div className='flex items-center justify-center gap-2.5'>
-                    <div className='mb-2 font-semibold text-base_dark'>Description :</div>
+                
+                <div className='relative w-full'>
+                    <div className=' flex-shrink-0 mb-2 font-semibold text-base_dark'>Description :</div>
                     <input
                       type='text'
                       {...register('description')}
-                      className='input-bordered  rounded-md border-2 p-1.5 text-black'
+                      className='input-bordered rounded-md w-full border-2 p-1.5 text-black'
                     />
                     {errors['description'] && (
-                      <div className='mt-2 text-wrap text-xs font-semibold text-danger'>
+                      <div className='text-wrap text-xs font-semibold text-danger'>
                         {errors['description'].message}
                       </div>
                     )}
-                  </div>
                 </div>
-                
-              </div>
-              <div className='flex flex-col items-start justify-start gap-4'>
               
-                <div className='inline-flex items-center justify-center gap-2.5'>
-                    <div className='mb-2 font-semibold text-base_dark'>Firstname :</div>
+                <div className='relative w-full'>
+                    <div className='flex-shrink-0 mb-2 w-[100px] font-semibold text-base_dark'>Firstname :</div>
                     <input
                       type='text'
                       {...register('firstName')}
-                      className='input-bordered rounded-md border-2 p-1.5 text-black'
+                      className='input-bordered rounded-md w-full border-2 p-1.5 text-black'
                     />
                     {errors['firstName'] && (
-                      <div className='mt-2 text-wrap text-xs font-semibold text-danger'>
+                      <div className='text-wrap text-xs font-semibold text-danger'>
                         {errors['firstName'].message}
                       </div>
                     )}
-                  
                 </div>
-                <div className='inline-flex items-center justify-center gap-2.5'>
+
+                <div className='relative w-full'>
                     <div className='mb-2 font-semibold text-base_dark'>Lastname :</div>
                     <input
                       type='text'
                       {...register('lastName')}
-                      className='input-bordered rounded-md border-2 p-1.5 text-black'
+                      className='input-bordered rounded-md w-full border-2 p-1.5 text-black'
                     />
                     {errors['lastName'] && (
-                      <div className='mt-2 text-wrap text-xs font-semibold text-danger'>
+                      <div className=' text-wrap text-xs font-semibold text-danger'>
                         {errors['lastName'].message}
                       </div>
                     )}
                 </div>
-                <DialogFooter>
-                    <Button type="submit">Save changes</Button>
+
+                <DialogFooter className="flex justify-end w-full ">
+                    <Button className="bg-pink-500" type="submit">Save changes</Button>
                 </DialogFooter>
                 {/* <button type="submit" className=" border-2 p-2">
                   Save Edit
                 </button> */}
               </div>
-            </div>
+            
           </form>
       </DialogContent>
     </Dialog>
