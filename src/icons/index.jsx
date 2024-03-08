c82fff5e9059b8575c86b6806dc5d4197aeba3f5
FlukeSK <146964113+FlukeSK@users.noreply.github.com>
2024-03-08 19:59:40 +0700

    Edithome

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -40,6 +40,26 @@ export function ChevronDownIcon({ className = null }) {
   );
 }
 
+export function SearchIcon({ className = null }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <circle cx='11' cy='11' r='8' />
+      <path d='m21 21-4.3-4.3' />
+    </svg>
+  );
+}
+
 export function HeartIcon({ className = null }) {
   return (
     <svg
@@ -55,6 +75,48 @@ export function HeartIcon({ className = null }) {
       className={className}
     >
       <path d='M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z' />
+    </svg>
+  );
+}
+
+export function ChevronRightIcon({ className = null }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path d='m9 18 6-6-6-6' />
+    </svg>
+  );
+}
+
+export function RabbitIcon({ className = null }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path d='M13 16a3 3 0 0 1 2.24 5' />
+      <path d='M18 12h.01' />
+      <path d='M18 21h-8a4 4 0 0 1-4-4 7 7 0 0 1 7-7h.2L9.6 6.4a1 1 0 1 1 2.8-2.8L15.8 7h.2c3.3 0 6 2.7 6 6v1a2 2 0 0 1-2 2h-1a3 3 0 0 0-3 3' />
+      <path d='M20 8.54V4a2 2 0 1 0-4 0v3' />
+      <path d='M7.612 12.524a3 3 0 1 0-1.6 4.3' />
     </svg>
   );
 }
@@ -153,4 +215,3 @@ export function QRIcon({ className = null }) {
     </svg>
   );
 }
-
