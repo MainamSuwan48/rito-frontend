4d1a9af15e295a2617100b6d31190ef40865c176
MainamSuwan48 <mainamsuwan48@gmail.com>
2024-03-09 00:12:57 +0700

    added small paddings

--- src/pages/UserPage/game-publishing/GameTagsCreator.jsx
+++ src/pages/UserPage/game-publishing/GameTagsCreator.jsx
@@ -18,7 +18,7 @@ function GameTagsCreator() {
   return (
     <div
       onClick={test}
-      className='flex w-full flex-col rounded-t-md bg-base-300'
+      className='flex w-full flex-col rounded-t-md bg-base-300 p-2 gap-1'
     >
       <div className='flex min-h-12 gap-2 p-2 items-start justify-start flex-wrap align-content-start'>
         {gameTagsForPublishing.map((tag, index) => (
