a7ac63a3d76496694fbf897161b6f4089d525693
FlukeSK <146964113+FlukeSK@users.noreply.github.com>
2024-03-08 21:46:26 +0700

    UpdateUIHomePage Wait addLogic

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -98,6 +98,64 @@ export function ChevronRightIcon({ className = null }) {
   );
 }
 
+export function FacebookIcon({ className = null }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path d='M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z' />
+    </svg>
+  );
+}
+
+export function TwitchIcon({ className = null }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path d='M21 2H3v16h5v4l4-4h5l4-4V2zm-10 9V7m5 4V7' />
+    </svg>
+  );
+}
+
+export function YoutubeIcon({ className = null }) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path d='M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17' />
+      <path d='m10 15 5-3-5-3z' />
+    </svg>
+  );
+}
+
 export function RabbitIcon({ className = null }) {
   return (
     <svg
--- src/pages/MainPage/HomePage.jsx
+++ src/pages/MainPage/HomePage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import LOAD from '../../assets/Img/RITO GAME LOADING.gif';
-import { ChevronRightIcon, RabbitIcon } from '@/icons';
+import {
+  ChevronRightIcon,
+  RabbitIcon,
+  FacebookIcon,
+  TwitchIcon,
+  YoutubeIcon,
+} from '@/icons';
+import BGCITY from '../../assets/Img/BG-CITY.png';
 
 function HomePage() {
   const navigate = useNavigate();
@@ -33,7 +40,7 @@ function HomePage() {
     },
   ];
   return (
-    <div className=''>
+    <div>
       {/* ============== < IMG HEADER > ============== */}
       <div className=' flex overflow-hidden bg-base_dark '>
         {arr.map((item, index) => {
@@ -48,59 +55,57 @@ function HomePage() {
         })}
       </div>
 
-      {/* <div class="bg-fixed ..." style="background-image: "> */}
-      {/* ============== < TITLE BEST GAME > ============== */}
-      <div className='mt-20 flex-col'>
-        <p className=' flex justify-center text-7xl font-bold text-primary hover:text-secondary_mute'>
-          BEST GAME IN YEAR 2024 <RabbitIcon />
-        </p>
-        <p className=' flex justify-center text-2xl font-bold'>
-          Play brand new games on day one from RITO Game Store and Bethesda
-          Softworks, plus selected indies and blockbusters.
-        </p>
-      </div>
-      {/* ============== < TITLE BEST GAME BUTTON > ============== */}
-      <div className='flex justify-center'>
-        <button>
-          <p className=' flex items-center gap-1 text-xl font-bold text-secondary_mute hover:gap-3 hover:text-primary'>
-            EXPLORE THE CATALOGUE <ChevronRightIcon />
+      <div className='p-[5rem]' style={{ backgroundImage: `url(${BGCITY})` }}>
+        {/* ============== < TITLE BEST GAME > ============== */}
+        <div className=' flex-col'>
+          <p className=' flex justify-center text-7xl font-bold text-purple-500 hover:scale-105 hover:text-primary'>
+            BEST GAME IN YEAR 2024 <RabbitIcon />
           </p>
-        </button>
-      </div>
-      {/* ============== < IMG BEST GAME > ============== */}
-      <div className=' m-8 flex flex-col justify-center'>
-        <button className='flex gap-[4rem]'>
-          <img
-            className='m-10 h-[20rem] w-[20rem] hover:scale-110'
-            src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/557703a2-f0df-4e9e-8006-f1a8a0666122/ddonp58-58ed224c-f931-496d-969a-2f7379849ef8.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU1NzcwM2EyLWYwZGYtNGU5ZS04MDA2LWYxYThhMDY2NjEyMlwvZGRvbnA1OC01OGVkMjI0Yy1mOTMxLTQ5NmQtOTY5YS0yZjczNzk4NDllZjgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.bdeXXP5-vduyFZpwzARVpt55CjfxHYDzKDsa2viZhE8'
-          />
-          <img
-            className='m-10 h-[20rem] w-[20rem] hover:scale-110'
-            src='https://th.bing.com/th/id/OIP.6RAx47-LD054F72piNM0YAHaF8?rs=1&pid=ImgDetMain'
-          />
-          <img
-            className='m-10 h-[20rem] w-[20rem] hover:scale-110'
-            src='https://www.freeiconspng.com/uploads/csgo-icon-4.png'
-          />
-          <img
-            className='m-10 h-[20rem] w-[20rem] hover:scale-110'
-            src='https://th.bing.com/th/id/R.55a7be8ae9cdaf82bfff3b98b021d328?rik=jUzMfKIl5FT1TA&pid=ImgRaw&r=0'
-          />
-        </button>
+          <p className=' flex justify-center text-2xl font-bold'>
+            Play brand new games on day one from RITO Game Store and Bethesda
+            Softworks, plus selected indies and blockbusters.
+          </p>
+        </div>
+        {/* ============== < TITLE BEST GAME BUTTON > ============== */}
+        <div className='flex justify-center'>
+          <button>
+            <p className=' flex items-center gap-1 text-xl font-bold text-secondary_mute hover:gap-3 hover:text-primary'>
+              EXPLORE THE CATALOGUE <ChevronRightIcon />
+            </p>
+          </button>
+        </div>
+        {/* ============== < IMG BEST GAME > ============== */}
+        <div className=' m-8 flex flex-col justify-center'>
+          <button className='flex gap-[4rem]'>
+            <img
+              className='m-10 h-[20rem] w-[20rem] hover:scale-110'
+              src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/557703a2-f0df-4e9e-8006-f1a8a0666122/ddonp58-58ed224c-f931-496d-969a-2f7379849ef8.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU1NzcwM2EyLWYwZGYtNGU5ZS04MDA2LWYxYThhMDY2NjEyMlwvZGRvbnA1OC01OGVkMjI0Yy1mOTMxLTQ5NmQtOTY5YS0yZjczNzk4NDllZjgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.bdeXXP5-vduyFZpwzARVpt55CjfxHYDzKDsa2viZhE8'
+            />
+            <img
+              className='m-10 h-[22rem] w-[22rem] hover:scale-110'
+              src='https://cos-1308089331.cos.accelerate.myqcloud.com/user_files/1/bbs/92264040_1647708448.png'
+            />
+            <img
+              className='m-10 h-[20rem] w-[20rem] hover:scale-110'
+              src='https://www.freeiconspng.com/uploads/csgo-icon-4.png'
+            />
+            <img
+              className='m-10 h-[20rem] w-[20rem] hover:scale-110'
+              src='https://th.bing.com/th/id/R.55a7be8ae9cdaf82bfff3b98b021d328?rik=jUzMfKIl5FT1TA&pid=ImgRaw&r=0'
+            />
+          </button>
+        </div>
       </div>
 
       {/* ============== < SHOW GAME 1 > ============== */}
-      <div className='mt-[5rem] flex h-auto w-auto bg-primary hover:bg-secondary_mute '>
+      <div className=' flex h-auto w-auto bg-base_dark '>
         <img
           className='h-[160%] w-[160%]'
           src='https://i.pinimg.com/originals/e5/4a/fa/e54afabd75adb33464e85f2687b43f87.gif'
         />
         <div className='p-[8rem]'>
-          <button className='flex-col justify-center text-3xl text-neutral  hover:text-primary'>
+          <button className='flex-col justify-center text-3xl text-neutral hover:scale-150 hover:text-purple-500'>
             Cyberpunk 2077
-          </button>
-          <button className='flex-col justify-center text-3xl text-neutral ml-6 hover:text-primary'>
-            GTA V
           </button>
           <br /> <br />
           <p className=' text-neutral'>
@@ -110,10 +115,69 @@ function HomePage() {
             Edgerunners — there are countless stories to discover in the deadly
             megalopolis of Night City.
           </p>
+          {/* ============== < Cyber GAME BUTTON > ============== */}
+          <div className='flex justify-center'>
+            <button>
+              <p className=' flex items-center gap-1 text-xl font-bold text-primary hover:gap-3 hover:text-neutral'>
+                OPEN TO RITO STORE <ChevronRightIcon />
+              </p>
+            </button>
+          </div>
         </div>
       </div>
+
+      {/* ============== < PLATFORM GAME > ============== */}
+      <div className='p-[5rem]' style={{ backgroundImage: `url(${BGCITY})` }}>
+        {/* ============== < TITLE BEST GAME > ============== */}
+        <div className=' flex-col'>
+          <p className=' flex justify-center text-7xl font-bold text-purple-500 hover:scale-105 hover:text-primary'>
+            BUILT FOR PC GAMERS <RabbitIcon />
+          </p>
+          <p className=' flex justify-center text-2xl font-bold'>
+            Finding your next favorite game has never been easier. Browse the
+            catalog View recommendations Filter by category
+          </p>
+        </div>
+        {/* ============== < TITLE BEST GAME BUTTON > ============== */}
+        <div className='flex justify-center'>
+          <button>
+            <p className=' flex items-center gap-1 text-xl font-bold text-secondary_mute hover:gap-3 hover:text-primary'>
+              DOWNLOAD RITO APP <ChevronRightIcon />
+            </p>
+          </button>
+        </div>
+      </div>
+
+      {/* ============== < SHOW GAME 2 > ============== */}
+      <div className=' flex h-auto w-auto bg-base_dark '>
+        <div className='p-[8rem]'>
+          <button className='flex-col justify-center text-3xl text-neutral hover:scale-150 hover:text-purple-500'>
+            Grand Theft Auto V
+          </button>
+          <br /> <br />
+          <p className=' text-neutral'>
+            When a young street hustler, a retired bank robber and a terrifying
+            psychopath find themselves entangled with some of the most
+            frightening and deranged elements of the criminal underworld, the
+            U.S. government and the entertainment industry, they must pull off a
+            series of dangerous heists to survive in a ruthless city in which
+            they can trust nobody, least of all each other.
+          </p>
+          {/* ============== < GTAV GAME BUTTON > ============== */}
+          <div className='flex justify-center'>
+            <button>
+              <p className=' flex items-center gap-1 text-xl font-bold text-primary hover:gap-3 hover:text-neutral'>
+                OPEN TO RITO STORE <ChevronRightIcon />
+              </p>
+            </button>
+          </div>
+        </div>
+        <img
+          className='h-[160%] w-[160%]'
+          src='https://www.bing.com/th/id/OGC.f7fe766005006db5d38d82d3777a695b?pid=1.7&rurl=https%3a%2f%2fmedia1.tenor.com%2fimages%2ff7fe766005006db5d38d82d3777a695b%2ftenor.gif%3fitemid%3d16275156&ehk=a4M8n5sOpKPh1ib972%2bgky5mgz0do9cGZ6K%2fPkhx%2fTU%3d'
+        />
+      </div>
     </div>
-    // </div>
   );
 }
 
