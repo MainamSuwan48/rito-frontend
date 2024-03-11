65f03a2427f7732f64e0c95eee6d116706482d84
bennytakdanai <takdanai.ht@gmail.com>
2024-03-11 12:19:07 +0700

    click on friend and navigate

--- src/features/user/components/FriendItems.jsx
+++ src/features/user/components/FriendItems.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+
 export default function FriendItems({friend}) {
+  const navigate = useNavigate()
+  const handleClick = ()=>{navigate(`/user/${friend.id}`)}
   return (
     <>
       <div className='inline-flex h-fit w-full items-center justify-between gap-24 border-b border-black bg-slate-400'>
         <div className='flex items-center justify-center gap-4'>
           <div className='flex items-center justify-center gap-2.5 bg-zinc-100 p-4'>
-            <div className='text-center text-4xl font-semibold text-black'>
-              P
+            <div 
+              className='text-center text-4xl font-semibold text-black cursor-pointer'
+              onClick={handleClick}
+            >
+            {friend.displayName.toUpperCase()[0]}
             </div>
           </div>
           <div className='inline-flex flex-col items-start justify-start gap-1.5'>
@@ -13,7 +21,7 @@ export default function FriendItems({friend}) {
               {friend.displayName}
             </div>
             <div className='text-center  text-xs font-normal leading-snug text-zinc-700'>
-              Pending Request
+              {friend.username}
             </div>
           </div>
         </div>
@@ -25,3 +33,5 @@ export default function FriendItems({friend}) {
     </>
   );
 }
+
+
