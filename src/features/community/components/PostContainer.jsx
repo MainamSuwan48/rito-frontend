5bf9a7d23b909e671d603304a83b6e8564922ec8
“puttpoomgit config --global user.email “poom.bm@gmail.com <poom@poomMAC.home>
2024-03-12 17:48:39 +0700

    add mockData and map into PostContainer

--- src/features/community/components/PostContainer.jsx
+++ src/features/community/components/PostContainer.jsx
@@ -1,14 +1,15 @@
-
 import CommunitySearchBar from './CommunitySearchBar';
 import PostItems from './PostItems';
+import mockData from './mockData.json';
 
 export default function PostContainer() {
   return (
     <>
-      <CommunitySearchBar />
+      {/* <CommunitySearchBar */}
       <div className=' flex flex-col gap-2.5 '>
-        <PostItems />
-        <PostItems />
+        {mockData.map((post, index) => (
+          <PostItems key={index} post={post} />
+        ))}
       </div>
     </>
   );
