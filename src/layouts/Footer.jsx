691beadad055624dbdb333903f4fcd618fc7d08b
“puttpoomgit config --global user.email “poom.bm@gmail.com <poom@poomMAC.home>
2024-03-13 21:39:42 +0700

    style-footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className='flex items-center justify-center p-4 text-[12px] text-base_dark text-opacity-50'>
+      © Rito Games Store 2024
+    </div>
+  );
 }
