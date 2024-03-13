02425c86487680292962096e5e8b75b61f2836f0
“puttpoomgit config --global user.email “poom.bm@gmail.com <poom@poomMAC.home>
2024-03-13 14:40:51 +0700

    add formatPrice in GamePageHero

--- src/features/store/game-page/components/GamePageHero.jsx
+++ src/features/store/game-page/components/GamePageHero.jsx
@@ -7,10 +7,13 @@ function GamePageHero({ gameData }) {
   return (
     <div className='flex flex-col'>
       <div className='flex items-center justify-between px-8'>
-        <div className='font-rubik text-primary-100 flex h-full items-start text-5xl font-extrabold'>
+        <div className='text-primary-100 flex h-full items-start font-rubik text-5xl font-extrabold'>
           {name}
         </div>
-        <div className='font-rubik text-[42px] font-bold text-primary bg-base_dark p-2 rounded-md bg-opacity-80'>{price}{" "}฿</div>
+        <div className='bg-base_dark bg-opacity-80 p-2 font-rubik text-[42px] font-bold text-primary'>
+          {new Intl.NumberFormat().format(price)}
+          {/* {price} ฿ */}
+        </div>
       </div>
       <div className='text-primary-100 flex h-full w-full items-start justify-center gap-4 p-8'>
         <GamePageCarousel mainImage={backgroundImageUrl} images={screenshots} />
--- src/pages/MainPage/HomePage.jsx
+++ src/pages/MainPage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ritoLogo from '../../assets/Img/ritoLogo.png';
+import mockGame from './mockGame.json';
 
 import { motion } from 'framer-motion';
 import {
@@ -15,39 +16,12 @@ import ScrollText from './ScrollText';
 
 function HomePage() {
   const navigate = useNavigate();
-  const arr = [
-    {
-      id: 3287,
-      name: 'Game1',
-      src: 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fbatman-arkham-knight%2FEGS_WB_Batman_Arkham_Knight_G1_1920x1080_19_0911-1920x1080-1d69e15f00cb5ab57249f208f1f8f45d52cbbc59.jpg',
-    },
-    {
-      id: 4291,
-      name: 'Game2',
-      src: 'https://videogiochitalia.it/wp-content/uploads/2023/09/counter-strike-2-come-giocare.webp',
-    },
-    {
-      id: 4161,
-      name: 'Game3',
-      src: 'https://assetsio.reedpopcdn.com/135430220067.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp',
-    },
-    {
-      id: 58175,
-      name: 'Game4',
-      src: 'https://media.wired.com/photos/639bf35a24c352e627eccc43/master/pass/Ragnaro%CC%88k-culture-ar1qdh.jpg',
-    },
-    {
-      id: 3144,
-      name: 'Game5',
-      src: 'https://img.gg.deals/65/91/5991c0b4e9b286c52d9d2270ffbf932915f1_912cr476.jpg',
-    },
-  ];
 
   return (
     <div>
       {/* ============== < IMG HEADER > ============== */}
       <div className=' flex overflow-hidden bg-base_dark '>
-        {arr.map((item, index) => {
+        {mockGame.map((item, index) => {
           return (
             <div
               onClick={() => navigate(`/game/${item.id}`)}
