85f76b578f50b3024f06ffa5610c109dde6655f4
MainamSuwan48 <mainamsuwan48@gmail.com>
2024-03-18 18:18:48 +0700

    clicking game tag in game card strip now navigate to search page

--- src/features/store/components/GameCardTag.jsx
+++ src/features/store/components/GameCardTag.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { makeSlug } from '@/utils/make-slug';
 import { useDispatch } from 'react-redux';
 import { searchGames, clearSearch } from '@/redux/slice/games-slice';
+import { useNavigate } from 'react-router-dom';
+
 
 function GameCardTag({ children, name }) {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const slugQuery = makeSlug(name);
   const search = (query) => {
     dispatch(searchGames(query));
+    navigate(`/search/`);
   };
   return (
     <button
